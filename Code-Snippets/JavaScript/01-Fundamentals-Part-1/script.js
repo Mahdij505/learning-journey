e9797526e8f85d8d00ae1f0d65eac5f0962c1e8d
Mahdi Jabbary <MahdiJabbary505@gmail.com>
2023-07-18 18:02:30 +0330

    JavaScript fundamentals part 1.

---
+++ Code-Snippets/JavaScript/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,75 @@
+/*
+const avgD = (96 + 108 + 89) / 3;
+const avgK = (88 + 91 + 110) / 3;
+
+if (avgD > avgK) {
+  console.log(
+    `Dolfins team is the winner of the competition with average of ${avgD}.`
+  );
+} else if (avgD < avgK) {
+  console.log(
+    `Koalas team is the winner of the competition with average of ${avgK}.`
+  );
+} else {
+  console.log(
+    `Both teams have got same average scores and the game is draw with average of ${avgD}.`
+  );
+}
+
+const avgD = (97 + 112 + 101) / 3;
+const avgK = (109 + 95 + 123) / 3;
+
+if (avgD >= 100 && avgD > avgK) {
+  console.log(
+    `Dolfins team is the winner of the competition with average of ${avgD}.`
+    );
+  } else if (avgK >= 100 && avgD < avgK) {
+    console.log(
+      `Koalas team is the winner of the competition with average of ${avgK}.`
+      );
+    } else if (avgD >= 100 && avgK >= 100 && avgD === avgK) {
+      console.log(
+        `Both teams have got same average scores and the game is draw with average of ${avgD}.`
+        );
+      } else if (avgD <= 100 && avgK <= 100) {
+  console.log(
+    `Both teams have got score less than 100 so there is no winner for compitition.`
+    );
+  }
+
+
+
+
+
+
+  const avgD = (97 + 112 + 101) / 3;
+const avgK = (109 + 95 + 106) / 3;
+
+if (avgD >= 100 && avgD > avgK) {
+  console.log(
+    `Dolfins team is the winner of the competition with average of ${avgD}.`
+    );
+  } else if (avgK >= 100 && avgD < avgK) {
+    console.log(
+      `Koalas team is the winner of the competition with average of ${avgK}.`
+      );
+    } else if (avgD >= 100 && avgK >= 100 && avgD === avgK) {
+      console.log(
+        `Both teams have got same average scores and the game is draw with average of ${avgD}.`
+        );
+      } else if (avgD <= 100 && avgK <= 100) {
+  console.log(
+    `Both teams have got score less than 100 so there is no winner for compitition.`
+    );
+  }
+  
+  */
+/*
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const final = bill + tip;
+
+console.log(
+  `The bill is ${bill}, The tip is ${tip}, and The final value is ${final}.  `
+);
+*/
